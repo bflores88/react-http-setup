76de8695bdba86ae8bcfb5658e256035688d24d2
kmietotwy <kmietotwy@gmail.com>
2019-08-01 13:55:19 -0700

    get call to jsonplaceholder.  renders title to posts using state and props

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,21 +6,28 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: []
+  }
 
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response);
+        // console.log(response);
+        this.setState({
+          posts: response.data
+        })
       });
   }
 
-    render () {
+  render() {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title}/>
+    });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
