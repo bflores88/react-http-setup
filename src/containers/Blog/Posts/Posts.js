81355a09b4ea45fc41e65ca15826b6586778ff07
kmietotwy <kmietotwy@gmail.com>
2019-08-02 14:39:45 -0700

    navigating programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class Posts extends Component {
 	state = {
@@ -32,22 +32,25 @@ class Posts extends Component {
 	}
 
 	postSelectedHandler = (id) => {
-		this.setState({
-			selectedPostId: id,
-		});
+		// this.setState({
+		// 	selectedPostId: id,
+    // });
+    this.props.history.push({
+      pathname: '/' + id
+    })
 	};
 
 	render() {
 		const posts = this.state.posts.map((post) => {
 			return (
-				<Link to={'/' + post.id} key={post.id}>
+				// <Link to={'/' + post.id} key={post.id}>
 					<Post
-						
+            key={post.id}
 						title={post.title}
 						author={post.author}
 						clicked={() => this.postSelectedHandler(post.id)}
 					/>
-				</Link>
+				// </Link>
 			);
 		});
 
