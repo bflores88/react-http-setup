d80b85eb3d0028be11d513d34e4d357af899de79
kmietotwy <kmietotwy@gmail.com>
2019-08-05 09:36:32 -0700

    implement lazy load in react using hoc asyncComponent

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
+
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
 	render() {
@@ -48,7 +54,7 @@ class Blog extends Component {
         <Route path="/" render={() => <h1>Home 2</h1>} />
       <Route path="/new-post" exact render={() => <h1>New Post</h1>} /> */}
 				<Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Not found</h1>} />
           {/* <Redirect from="/" to ="/posts" /> */}
